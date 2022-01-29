0d5d5311f7be1e2673e40fea73aad52d4ca76979
Rob <rrealmuto@gmail.com>
2022-01-29 01:05:02 -0500

    Add a bunch of overworld pots. Need to test still

--- Patches.py
+++ Patches.py
@@ -1454,7 +1454,9 @@ def patch_rom(spoiler:Spoiler, world:World, rom:Rom):
     # Get actor_override locations
         actor_override_locations = [location for location in world.get_locations() if location.disabled == DisableType.ENABLED and location.type == 'ActorOverride' ]
         freestanding_locations = [location for location in world.get_locations() if location.disabled == DisableType.ENABLED and location.type == 'Collectable' and 'Freestanding' in location.filter_tags]
-        for location in actor_override_locations + freestanding_locations:
+        pot_locations = [location for location in world.get_locations() if location.disabled == DisableType.ENABLED and location.type == 'Collectable' and 'Pot' in location.filter_tags]
+        
+        for location in actor_override_locations + freestanding_locations + pot_locations:
                 addresses = location.address
                 patch = location.address2
                 if addresses is not None and patch is not None:
