267513bb117bc197d6732538da5fb66f3f5cf794
Rob Realmuto <rrealmuto@gmail.com>
2022-02-01 21:38:57 -0500

    Hack to erase new collectible flags in SRAM when initializing a new save. Copying still wont work

--- ASM/c/save.c
+++ ASM/c/save.c
@@ -3,7 +3,7 @@
 
 #define SRAM_BASE 0x08000000
 #define SRAM_NEWDATA_START 0x65B0
-
+#define SRAM_SIZE 0x8000
 extern uint32_t collectible_override_flags[202];
 extern uint32_t dropped_collectible_override_flags[202];
 
@@ -21,4 +21,14 @@ void Save_Open(char* sramBuffer)
 {
     z64_memcopy(&collectible_override_flags, sramBuffer + SRAM_NEWDATA_START + ((z64_file.file_index) * sizeof(uint32_t)*202), sizeof(uint32_t)*202);
     z64_memcopy(&dropped_collectible_override_flags, sramBuffer + SRAM_NEWDATA_START + (2*sizeof(uint32_t) *202) + ((z64_file.file_index) * sizeof(uint32_t)*202), sizeof(uint32_t)*202);
+}
+
+void Save_Init_Write_Hook(uint32_t addr, void* dramAddr, size_t size, uint32_t direction)
+{
+    //zeroize the new collectible flags in the sram buffer (dramAddr)
+    z64_bzero(dramAddr + SRAM_NEWDATA_START + ((z64_file.file_index) * sizeof(uint32_t)*202), sizeof(uint32_t) * 202 );
+    z64_bzero(dramAddr + SRAM_NEWDATA_START + (2*sizeof(uint32_t) *202) + ((z64_file.file_index) * sizeof(uint32_t)*202),sizeof(uint32_t) *202);
+    
+    //write to sram
+    SsSram_ReadWrite(SRAM_BASE, dramAddr, SRAM_SIZE, direction);
 }
