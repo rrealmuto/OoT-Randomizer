f6b335c2566b5c6fb52a25ba6f36f7f07c7f5082
Rob Realmuto <rrealmuto@gmail.com>
2022-02-14 13:34:55 -0500

    Change version number

--- version.py
+++ version.py
@@ -3,6 +3,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 __version__ = '6.2.29 f.LUM'
 =======
 __version__ = '6.2.19.rreal-1.0'
@@ -19,3 +20,6 @@ __version__ = '6.2.19.rreal-1.3'
 =======
 __version__ = '6.2.19.rreal-1.5'
 >>>>>>> 61f7397e (merged shadow pot and rupee towers. Update version and newchecks.txt)
+=======
+__version__ = '6.2.19 rreal-1.5'
+>>>>>>> 11b2c87b (Change version number)
