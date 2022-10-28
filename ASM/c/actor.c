55753133ccd6c75a22725e5a99861c93a4fe223b
Rob Realmuto <rrealmuto@gmail.com>
2022-10-28 18:58:00 -0400

    Make shuffled silver rupees not spawn as green rupees when they're collected

--- ASM/c/actor.c
+++ ASM/c/actor.c
@@ -1,4 +1,6 @@
 #include "z64.h"
+#include "actor.h"
+#include "get_items.h"
 
 #define BG_HAKA_TUBO        0x00BB
 #define BG_SPOT18_BASKET    0x015C
@@ -9,6 +11,8 @@
 #define EN_G_SWITCH         0x0117 //Silver Rupee
 
 extern uint8_t SHUFFLE_SILVER_RUPEES;
+extern uint16_t CURR_ACTOR_SPAWN_INDEX;
+extern int8_t curr_scene_setup;
 
 void Actor_SetWorldToHome_End(z64_actor_t *actor) {
     // Reset rotations to 0 for any actors that it gets passed into the spawn params
@@ -30,27 +34,38 @@ void Actor_SetWorldToHome_End(z64_actor_t *actor) {
 }
 
 z64_actor_t* Actor_SpawnEntry_Hack(void* actorCtx, ActorEntry* actorEntry, z64_game_t* globalCtx){
-    uint16_t newParams = actorEntry->params;
-    uint16_t newID = actorEntry->id;
-
+    bool continue_spawn = true;
     switch(actorEntry->id)
     {
         case(EN_G_SWITCH):
         {
-            if(SHUFFLE_SILVER_RUPEES)
-            {
-                uint8_t type = (actorEntry->params >> 0x0C) & 0xF;
-                if(type == 1) // only override actual silver rupees, not the switches or pots.
-                {
-                    newParams = 0;
-                    newID = EN_ITEM00;
-                }
-                break;
-            }
+            continue_spawn = spawn_override_silver_rupee(actorEntry, globalCtx);
+            break;
         }
         default:
             break;
     }
-    return z64_SpawnActor(actorCtx, globalCtx, newID, actorEntry->pos.x, actorEntry->pos.y, actorEntry->pos.z,
-                       actorEntry->rot.x, actorEntry->rot.y, actorEntry->rot.z, newParams);
+    z64_actor_t* spawned = NULL;
+    if(continue_spawn)
+    {
+        spawned = z64_SpawnActor(actorCtx, globalCtx, actorEntry->id, actorEntry->pos.x, actorEntry->pos.y, actorEntry->pos.z,
+            actorEntry->rot.x, actorEntry->rot.y, actorEntry->rot.z, actorEntry->params);
+    }
+    return spawned;   
+    
+}
+
+bool spawn_override_silver_rupee(ActorEntry* actorEntry, z64_game_t* globalCtx){
+    if(SHUFFLE_SILVER_RUPEES)
+    {
+        EnItem00 dummy;
+        dummy.actor.actor_id = 0x15;
+        dummy.actor.rot_init.y = (curr_scene_setup << 14) + (globalCtx->room_index << 8) + CURR_ACTOR_SPAWN_INDEX + 1;
+        uint8_t type = (actorEntry->params >> 0x0C) & 0xF;
+        if(type == 1 && should_override_collectible(&dummy)) // only override actual silver rupees, not the switches or pots.
+        {
+            actorEntry->params = 0;
+            actorEntry->id = EN_ITEM00;
+        }
+    }
 }
