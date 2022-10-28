5dd2b9bd9fc704c30ac49d5bd22d29fac0e0aee8
Rob Realmuto <rrealmuto@gmail.com>
2022-10-27 23:30:36 -0400

    use supplementary version

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
-__version__ = '6.2.203'
+__version__ = '6.2.202'
 
 # This is a supplementary version number for branches based off of main dev.
-supplementary_version = 0
+supplementary_version = 1
 
 # Pick a unique identifier byte for your fork if you are intending to have a long-lasting branch.
 # This will be 0x00 for main releases and 0x01 for main dev.
@@ -14,4 +14,4 @@ branch_url = 'https://github.com/rrealmuto/OoT-Randomizer/tree/Dev'
 base_version = __version__
 
 # And finally, the completed version string. This is what is displayed and used for salting seeds.
-__version__ = f'{base_version} rreal 2.0.0'
+__version__ = f'{base_version} RREAL-{supplementary_version}'
