8a0b60833f56d3c8b40adfde3f91b9c5c0ee3070
Fenhl <fenhl@fenhl.net>
2023-02-03 05:32:26 +0000

    Update web version messages for regular hotfixes

--- SettingsList.py
+++ SettingsList.py
@@ -1893,7 +1893,7 @@ setting_infos = [
     Setting_Info(
         name           = 'version_msg',
         type           = str,
-        gui_text       = f"Ocarina of Time Randomizer version {__version__}\nThis version may be outdated. Go to github.com/fenhl/OoT-Randomizer for the latest version.",
+        gui_text       = f"Ocarina of Time Randomizer version {__version__}",
         gui_type       = "Textbox",
         shared         = False,
         choices        = {},
