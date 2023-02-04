8dc39e88866203b68711aa3f5c6d7c3b02941d4d
engineer124 <engineer124engineer124@gmail.com>
2023-02-04 10:44:16 -0500

    Fix pokey trap

--- Patches.py
+++ Patches.py
@@ -802,6 +802,8 @@ def patch_rom(spoiler:Spoiler, world:World, rom:Rom):
     rom.write_bytes(0xE5400E, [0xB4, 0xA4])
     if world.settings.open_forest != 'closed':
         rom.write_bytes(0xE5401C, [0x14, 0x0B])
+    # Move Link spawn 40 units forwards to prevent Pokey trap
+    rom.write_byte(0x206F0C7, 0xA3)
 
     # Fix Shadow Temple to check for different rewards for scene
     rom.write_bytes(0xCA3F32, [0x00, 0x00, 0x25, 0x4A, 0x00, 0x10])
