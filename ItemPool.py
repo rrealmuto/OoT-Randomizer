80e92cdf58bdce0dce47dcf0e4462067a9ea9fb1
Rob Realmuto <rrealmuto@gmail.com>
2023-06-20 20:41:28 -0400

    Enemy Drop Shuffle

--- ItemPool.py
+++ ItemPool.py
@@ -752,6 +752,14 @@ def get_pool_core(world: World) -> tuple[list[str], dict[str, Item]]:
                 shuffle_item = False
                 location.disabled = DisableType.DISABLED
 
+        # Enemy Drops
+        elif location.type == 'EnemyDrop':
+            if world.settings.shuffle_enemy_drops:
+                shuffle_item = True
+            else:
+                shuffle_item = False
+                location.disabled = DisableType.DISABLED
+
         # Dungeon Items
         elif location.dungeon is not None:
             dungeon = location.dungeon
