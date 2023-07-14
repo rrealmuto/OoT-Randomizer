73d3791e9cb335a64489304ea4be6be489d5f29d
Fenhl <fenhl@fenhl.net>
2023-07-14 17:52:58 +0000

    update-presets: list-non-default subcommand; Support new SettingsList.py format; Unix support

--- update-presets.py
+++ update-presets.py
@@ -2,6 +2,7 @@
 
 Usage:
   update-presets.py [options]
+  update-presets.py list-non-default <preset>
   update-presets.py diff <left> <right>
   update-presets.py (-h | --help)
 
@@ -15,11 +16,17 @@ Options:
 import sys
 
 import json
+import platform
 import subprocess
 
 import docopt # PyPI: docopt
 
 import SettingsList
+
+if hasattr(SettingsList, 'si_dict'):
+    SETTINGS_DICT = SettingsList.si_dict
+else:
+    SETTINGS_DICT = SettingsList.SettingInfos.setting_infos
 
 def complete_presets(new_presets, interactive, *, preset=None, source=None):
     with open('data/presets_default.json', encoding='utf-8') as f:
@@ -38,7 +45,7 @@ def complete_presets(new_presets, interactive, *, preset=None, source=None):
         if preset_name not in new_presets:
             new_presets[preset_name] = {}
         SettingsList.validate_settings(new_presets[preset_name], check_conflicts=False)
-        for setting_name, setting in SettingsList.si_dict.items():
+        for setting_name, setting in SETTINGS_DICT.items():
             if setting.shared and setting_name not in new_presets[preset_name]:
                 if setting_name in old_preset:
                     try:
@@ -71,14 +78,19 @@ def complete_presets(new_presets, interactive, *, preset=None, source=None):
 if __name__ == '__main__':
     arguments = docopt.docopt(__doc__)
     new_presets = {}
-    if arguments['diff']:
+    if arguments['list-non-default']:
+        with open('data/presets_default.json', encoding='utf-8') as f:
+            preset = json.load(f)[arguments['<preset>']]
+        non_default = {name: value for name, value in preset.items() if value != SETTINGS_DICT[name].default}
+        print(json.dumps(non_default, indent=4))
+    elif arguments['diff']:
         if arguments['<left>'] == 'default':
-            left = {setting_name: setting.default for setting_name, setting in SettingsList.si_dict.items() if setting.shared}
+            left = {setting_name: setting.default for setting_name, setting in SETTINGS_DICT.items() if setting.shared}
         else:
             with open('data/presets_default.json', encoding='utf-8') as f:
                 left = json.load(f)[arguments['<left>']]
         if arguments['<right>'] == 'default':
-            right = {setting_name: setting.default for setting_name, setting in SettingsList.si_dict.items() if setting.shared}
+            right = {setting_name: setting.default for setting_name, setting in SETTINGS_DICT.items() if setting.shared}
         else:
             with open('data/presets_default.json', encoding='utf-8') as f:
                 right = json.load(f)[arguments['<right>']]
@@ -86,7 +98,8 @@ if __name__ == '__main__':
             json.dump(left, left_f, indent=4)
         with open('right.json', 'w', encoding='utf-8') as right_f:
             json.dump(right, right_f, indent=4)
-        sys.exit(subprocess.run(['wsl', 'diff', left_f.name, right_f.name]).returncode)
+        diff = ['wsl', 'diff'] if platform.system() == 'Windows' else ['diff']
+        sys.exit(subprocess.run([*diff, left_f.name, right_f.name]).returncode)
     elif arguments['--hook']:
         complete_presets(new_presets, False)
         with open('data/presets_default.json', encoding='utf-8') as f:
