45907ce5ac823ab65a8c28861e505113a4e27dd5
Rob Realmuto <rrealmuto@gmail.com>
2023-11-17 18:01:02 -0500

    Update vadpcm_enc_dir script

--- data/Voices/vadpcm_enc_dir.py
+++ data/Voices/vadpcm_enc_dir.py
@@ -3,11 +3,17 @@ import sys
 import subprocess
 
 if __name__ == '__main__':
-    pack_dir = sys.argv[1]
+    if len(sys.argv) < 2:
+        pack_dir = os.curdir
+    else:
+        pack_dir = sys.argv[1]
     print(pack_dir)
     files: list[str] = os.listdir(pack_dir)
+    count = 0
     for file in files:
         if file.endswith(".aiff"):
             filename = os.path.join(pack_dir,file)
             print(file)
             subprocess.call(['./vadpcm_enc', '-c', 'shared_pred.tbl', filename, filename+'.aifc'])
+            count += 1
+    print(f"Converted {count} AIFF files.")
