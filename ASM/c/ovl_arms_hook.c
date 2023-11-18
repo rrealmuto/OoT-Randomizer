8f1042d67ba7a0566eb489171939a35321bb6325
Rob Realmuto <rrealmuto@gmail.com>
2023-11-18 11:43:32 -0500

    Hack Player_DrawHookshotReticle to increase distance that the reticle draws

--- ASM/c/ovl_arms_hook.c
+++ ASM/c/ovl_arms_hook.c
@@ -3,6 +3,7 @@
 
 extern uint8_t HOOKSHOT_EXTENSIONS;
 extern void ArmsHook_Wait(ArmsHook* this, z64_game_t* globalCtx);
+extern void Player_DrawHookshotReticle(z64_game_t* globalCtx, z64_link_t* this, float arg2);
 
 void ArmsHook_Wait_Hooked(ArmsHook* this, z64_game_t* globalCtx) {
 
@@ -13,4 +14,11 @@ void ArmsHook_Wait_Hooked(ArmsHook* this, z64_game_t* globalCtx) {
     if(HOOKSHOT_EXTENSIONS) {
         this->timer = extended_savectx.hookshot_level * 13;
     }
+}
+
+void Player_DrawHookshotReticle_Hook(z64_game_t* globalCtx, z64_link_t* this, float arg2) {
+    if(HOOKSHOT_EXTENSIONS) {
+        arg2 = extended_savectx.hookshot_level * 38800.0;
+    }
+    Player_DrawHookshotReticle(globalCtx, this, arg2);
 }
--- ASM/src/hacks/ovl_arms_hook.asm
+++ ASM/src/hacks/ovl_arms_hook.asm
@@ -1,5 +1,6 @@
 ; Hookshot hacks
 
+; Hacks in arms_hook overlay
 .headersize(0x8090fb40 - 0xcad2c0)
 
 ; Custom hookshot length function
@@ -12,3 +13,13 @@
     j       ArmsHook_Wait_Hooked
     nop
 Arms_HookWait_Continue:
+
+; Hacks in code file related to hookshot
+.headersize (0x800110A0 - 0xA87000)
+
+; Hook call to Player_DrawHookshotReticle to set the distance for the reticle check
+; At call to Player_DrawHookshotReticle in Player_PostLimbDrawGameplay
+.org 0x8007bec8
+; Replaces
+;   jal   0x8007b560 ; call Player_DrawHookshotReticle
+    jal   Player_DrawHookshotReticle_Hook
