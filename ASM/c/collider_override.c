62100801aa7c771df779c566f445c9cacd5f48dc
Rob Realmuto <rrealmuto@gmail.com>
2024-01-27 16:16:16 -0500

    Scale boulders with overridden colliders appropriately. Update brown boulder explosions to use collider

--- ASM/c/collider_override.c
+++ ASM/c/collider_override.c
@@ -26,9 +26,9 @@ typedef struct collidercylinder_override {
 } collidercylinder_override;
 
 collidercylinder_override collidercylinder_override_table[] = {
-    { .scene = 0x58, .pad = 0, .setup = 2, .room = 1, .flag = 16, .radius_override = 70 }, // ZD Shop red ice
-    { .scene = 0x09, .pad = 0, .setup = 0, .room = 3, .flag = 18, .radius_override = 70 },
-    { .scene = 0x09, .pad = 0, .setup = 0, .room = 3, .flag = 19, .radius_override = 70 },
+    { .scene = 0x58, .pad = 0, .setup = 2, .room = 1, .flag = 16, .radius_override = 80 }, // ZD Shop red ice
+    { .scene = 0x09, .pad = 0, .setup = 0, .room = 3, .flag = 18, .radius_override = 80 }, // Ice cavern red ice wall
+    { .scene = 0x09, .pad = 0, .setup = 0, .room = 3, .flag = 19, .radius_override = 80 }, // Ice cavern red ice wall
     { .scene = 0x00, .all = 0, .radius_override = 0 }
 };
 
@@ -37,7 +37,7 @@ extern uint16_t CURR_ACTOR_SPAWN_INDEX;
 int32_t Collider_SetCylinder_Hook(z64_game_t* globalCtx, ColliderCylinder* collider, z64_actor_t* actor, ColliderCylinderInit* src) {
     // check if our actor is in the collider override table
     collidercylinder_override* curr = &collidercylinder_override_table[0];
-    
+    ColliderCylinderInit copy_collider_init = *src;
     collidercylinder_override flag = { 0 };
     flag.scene = globalCtx->scene_index;
     flag.room = actor->room_index;
@@ -47,10 +47,17 @@ int32_t Collider_SetCylinder_Hook(z64_game_t* globalCtx, ColliderCylinder* colli
     {
         if(curr->scene == flag.scene && curr->all == flag.all)
         {
-            src->dim.radius = curr->radius_override;
+            // Get the different between the new radius and the original
+            float scale = (float)curr->radius_override / (float)src->dim.radius;
+            copy_collider_init.dim.radius = curr->radius_override;
+            // scale the actor
+            actor->scale.x *= scale;
+            actor->scale.y *= scale;
+            actor->scale.z *= scale;
+            break;
         }
         curr++;
     }
     
-    return Collider_SetCylinder(globalCtx, collider, actor, src);
+    return Collider_SetCylinder(globalCtx, collider, actor, &copy_collider_init);
 }
