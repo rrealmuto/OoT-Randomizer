110c1a8bbf4a078783b2d6848f6d29d03a3cfa7b
Rob Realmuto <rrealmuto@gmail.com>
2024-04-05 18:51:34 -0400

    Fix masks rendering while in first person

--- ASM/c/player.c
+++ ASM/c/player.c
@@ -6,6 +6,8 @@
 #define Player_DrawGameplay_Orig_Addr 0x80848200 // Original Player_DrawGameplay function which we'll call
 #define sMaskDlists_Addr 0x808526fc // sMaskDlists contains the displaylist to call for each mask
 
+extern int32_t Player_OverrideLimbDrawGameplayDefault(z64_game_t* play, int32_t limbIndex, Gfx** dList, z64_xyzf_t* pos, z64_xyz_t* rot, void*);
+
 Gfx* sMaskDlistsAdult[] = {0,0,0,0x06001010, 0,0,0,0};
 
 // Hook Player_DrawGameplay so we can draw our new bunny hood
@@ -14,7 +16,7 @@ void Player_DrawGameplay_Hook(z64_game_t* globalCtx, z64_link_t* this, int32_t l
     Player_DrawGameplay_Func Player_DrawGameplay = resolve_player_ovl_addr(Player_DrawGameplay_Orig_Addr);
     Player_DrawGameplay(globalCtx, this, lod, cullDList, overrideLimbDraw);
     z64_gfx_t* gfx = globalCtx->common.gfx;
-    if(this->current_mask)
+    if(overrideLimbDraw == Player_OverrideLimbDrawGameplayDefault && this->current_mask)
     {
         uint32_t* sMaskDlists;
         if(LINK_IS_ADULT) {
