429ecd5cbe68ed26a78acf148df4f4c7bc5e7dc5
Fenhl <fenhl@fenhl.net>
2024-05-10 02:25:21 +0000

    Create Cargo workspace

--- crate/rust-n64-test/assets/build.py
+++ crate/rust-n64-test/assets/build.py
@@ -3,9 +3,8 @@ import shutil
 import subprocess
 
 subprocess.run(
-    ['cargo', '+nightly', 'build', '-Zbuild-std=core', '-Zbuild-std-features=compiler-builtins-mem', '--release', '--target=assets/mips-ultra64-cpu.json'],
-    cwd='crate/rust-n64-test',
+    ['cargo', '+nightly', 'build', '-Zbuild-std=core', '-Zbuild-std-features=compiler-builtins-mem', '--release', '--package=rust-n64-test', '--target=crate/rust-n64-test/assets/mips-ultra64-cpu.json'],
     env={**os.environ, 'RUSTFLAGS': '-Csymbol-mangling-version=v0'}, # symbol-mangling-version (https://github.com/rust-lang/rust/issues/60705) required for armips to accept the symbols
     check=True,
 )
-shutil.copyfile('crate/rust-n64-test/target/mips-ultra64-cpu/release/librust_n64_test.a', 'ASM/build/librust_n64_test.a')
+shutil.copyfile('target/mips-ultra64-cpu/release/librust_n64_test.a', 'ASM/build/librust_n64_test.a')
