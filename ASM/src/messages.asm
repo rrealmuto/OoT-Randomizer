5b44824a578daf4b6c3d12eedeacc3eeda93481b
Vincent Cunningham <v.cunningham@flagrama.com>
2024-07-29 19:54:39 -0400

    Fixes the weird text alignment when going from a textbox with an icon to one with no icon

--- ASM/src/messages.asm
+++ ASM/src/messages.asm
@@ -129,3 +129,10 @@ shooting_gallery_no_bow:
 
     jr      ra
     addiu   sp, sp, 0x20
+
+Message_Decode_reset_msgCtx.textPosX:
+    sb      $zero, 0x4be(at)
+    sh      $zero, 0x4c0(at)
+    lhu     a3, 0x4c0(a3)
+    j       0x800da354
+    nop
