9799b8c3f6babe45a4e57fa8bd40e01327db5c4a
Fenhl <fenhl@fenhl.net>
2025-05-19 10:04:47 +0000

    Fix C compiler warning

--- ASM/c/z64.h
+++ ASM/c/z64.h
@@ -2464,6 +2464,6 @@ typedef void(*z64_Play_SetupRespawnPoint_proc)(z64_game_t *game, int32_t respawn
 
 extern void Fault_AddHungupAndCrashImpl(const char* msg1, const char* msg2);
 extern int32_t sprintf(char* dst, char* fmt, ...);
-extern int32_t DemoKankyo_CutsceneFlags_Get_Hook(void* play, int16_t flag);
+extern int32_t CutsceneFlags_Get(void* play, int16_t flag);
 
 #endif
