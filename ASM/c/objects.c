2173210ebffb646ea0b5e2cddea3b5755d45d993
Rob Realmuto <rrealmuto@gmail.com>
2025-05-26 10:24:06 -0400

    Remove enholl hacks and prevRoom checks from object system since they're not necessary anymore

--- ASM/c/objects.c
+++ ASM/c/objects.c
@@ -19,26 +19,29 @@ extern uint8_t CFG_OBJECT_SYSTEM;
 
 extended_object_ctx_t extended_object_ctx;
 
+int8_t gPrevRoom;
+
 void extended_objects_init() {
     //extended_object_ctx.heap = heap_alloc(0x20000);
     //extended_object_ctx.free = extended_object_ctx.heap;
     //extended_object_ctx.num = 0;
     //extended_object_ctx.size = 0x20000;
-    extended_object_ctx.holl_last_room = -1;
-    extended_object_ctx.inhibit_clear_flag = 0;
+    //extended_object_ctx.holl_last_room = -1;
+    //extended_object_ctx.inhibit_clear_flag = 0;
     for(int i = 0; i < OBJECT_EXCHANGE_BANK_EXTENDED_MAX; i++) {
         extended_object_ctx.slots[i].id = 0;
         extended_object_ctx.slots[i].is_active = 0;
         extended_object_ctx.slots[i].room = -1;
         extended_object_ctx.slots[i].data = NULL;
     }
+    gPrevRoom = -1;
 }
 
 void extended_objects_reset() {
     //extended_object_ctx.free = extended_object_ctx.heap;
     //extended_object_ctx.num = 0;
-    extended_object_ctx.holl_last_room = -1;
-    extended_object_ctx.inhibit_clear_flag = 0;
+    //extended_object_ctx.holl_last_room = -1;
+    //extended_object_ctx.inhibit_clear_flag = 0;
     for(int i = OBJECT_EXCHANGE_BANK_MAX; i < OBJECT_EXCHANGE_BANK_EXTENDED_MAX; i++) {
         extended_object_ctx.slots[i].id = 0;
         if(extended_object_ctx.slots[i].data) {
@@ -48,6 +51,7 @@ void extended_objects_reset() {
         extended_object_ctx.slots[i].room = -1;
         extended_object_ctx.slots[i].data = NULL;
     }
+    gPrevRoom = -1;
 }
 
 void Scene_CommandObjectList_Hook(z64_game_t* globalCtx, void* scene_command) {
@@ -68,32 +72,29 @@ typedef struct EnHoll {
     /* 0x0140 */ void* actionFunc;
 } EnHoll; // size = 0x0144
 
-void EnHoll_Room_Change_Hack(z64_game_t* globalCtx, RoomContext* roomCtx, EnHoll* holl) {
+/*void EnHoll_Room_Change_Hack(z64_game_t* globalCtx, RoomContext* roomCtx, EnHoll* holl) {
     if((holl->actor.room_index == extended_object_ctx.holl_last_room) || (holl->actor.room_index == roomCtx->curRoom.num)) {
         extended_object_ctx.inhibit_clear_flag = 1;
     }
     z64_UnloadRoom(globalCtx, roomCtx);
     extended_object_ctx.holl_last_room = holl->actor.room_index;
     extended_object_ctx.inhibit_clear_flag = 0;
-}
-
-int8_t gPrevRoom;
+}*/
 
 void Room_Change_Hook(z64_game_t* globalCtx, RoomContext* roomCtx) {
-    int8_t prevRoom = roomCtx->prevRoom.num;
-    gPrevRoom = prevRoom;
+    gPrevRoom = roomCtx->prevRoom.num;
     Room_Change(globalCtx, roomCtx);
     
-    if(extended_object_ctx.inhibit_clear_flag)
-        return;
+    //if(extended_object_ctx.inhibit_clear_flag)
+    //    return;
     
-    if(prevRoom >= 0){
+    //if(prevRoom >= 0){
         extended_object_t* slot = &extended_object_ctx.slots[OBJECT_EXCHANGE_BANK_MAX];
         for(int i = OBJECT_EXCHANGE_BANK_MAX; i < OBJECT_EXCHANGE_BANK_EXTENDED_MAX; i++) {
             if(slot->data && slot->is_active) {
                 slot->is_active = 0;
             }
-            else if(slot->room != roomCtx->curRoom.num) { // Don't unload the object if it is for the current room. Mostly sanity check to fix actor glitch crashing
+            else if(slot->room != roomCtx->curRoom.num) { // Don't unload the object if it is for the current room.
                 // The slot is no longer active so free the slot and the data from the heap
                 ZeldaArena_Free(slot->data);
                 slot->id = 0;
@@ -102,8 +103,7 @@ void Room_Change_Hook(z64_game_t* globalCtx, RoomContext* roomCtx) {
             }
             slot++;
         }
-    }
-    
+    //}
 }
 
 int32_t Object_GetIndex_Hook(z64_obj_ctxt_t *object_ctx, int16_t object_id) {
@@ -136,6 +136,7 @@ int32_t Object_GetIndex_Hook(z64_obj_ctxt_t *object_ctx, int16_t object_id) {
             // Load the object
             size = load_object_file(object_id, extended_object_ctx.slots[free_index].data);
             extended_object_ctx.slots[free_index].id = object_id;
+
             extended_object_ctx.slots[free_index].is_active = 1;
             extended_object_ctx.slots[free_index].room = z64_game.room_ctx.curRoom.num;
             //extended_object_ctx.free += size;
