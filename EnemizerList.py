523eba9d64efd14b59dd01240d0490775fb71dd7
Rob Realmuto <rrealmuto@gmail.com>
2025-06-06 12:22:18 -0400

    MQ Deku Enemizer Logic

--- EnemizerList.py
+++ EnemizerList.py
@@ -1252,6 +1252,8 @@ mq_dungeon_enemies = {
 }
 
 named_rooms: dict[str, tuple[int,int]] = {
+    'DEKU MQ SLINGSHOT ROOM': (0, 10),
+    'DEKU MQ BASEMENT LARVA ROOM': (0, 6),
     'KAK REDEAD GROTTO': (0x3E, 2),
     'ROYAL FAMILY TOMB ENTRYWAY': (0x41,0),
     'SFM WOLFOS GROTTO': (0x3E, 7),
