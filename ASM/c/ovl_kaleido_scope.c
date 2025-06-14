6aebe71a9d0105bb29dcd8ba1ef24c2a31860a46
Rob Realmuto <rrealmuto@gmail.com>
2025-06-13 20:59:51 -0400

    TBH not sure how this works

--- ASM/c/ovl_kaleido_scope.c
+++ ASM/c/ovl_kaleido_scope.c
@@ -3,6 +3,7 @@
 #include "text.h"
 #include "enemy_spawn_shuffle.h"
 #include "ovl_kaleidoscope.h"
+#include "util.h"
 
 #define KaleidoScope_DrawWorldMap   0x8081CE54
 #define KaleidoScope_DrawDungeonMap 0x8081b660
@@ -243,4 +244,65 @@ void PauseMapMark_Draw_CallHook(z64_game_t* globalCtx) {
     pause_mapmark_draw_func handler = KaleidoManager_GetRamAddr((void*)PauseMapMark_Draw);
     if(menu_page == 0)
         handler(globalCtx);
+}
+
+int32_t KaleidoScope_AllocAndDmaRequest(void** ppRam, uintptr_t vrom, size_t size) {
+    // Allocate the space
+    *ppRam = ZeldaArena_Malloc(size);
+    DmaMgr_RequestSync(*ppRam, vrom, size);
+}
+
+int32_t KaleidoScope_AllocAndDmaRequestNameSegment(void** ppRam, uintptr_t vrom, size_t size) {
+    // Allocate size + 0x400
+    *ppRam = ZeldaArena_Malloc(size + 0x400);
+
+    // DMA to ppRam + 0x400
+    DmaMgr_RequestSync(*ppRam + 0x400, vrom, size);
+}
+
+extern void PreRender_SetValuesSave(void* this, uint32_t width, uint32_t height, void* fbuf, void* zbuf, void* cvg);
+
+void KaleidoScope_PreRender_SetValuesSave_Hook(void* this, uint32_t width, uint32_t height, void* fbuf, void* zbuf, void** pcvg) {
+    // Allocate space for the cvg
+    *pcvg = ZeldaArena_Malloc(8*width*height);
+    PreRender_SetValuesSave(this, width, height, fbuf, zbuf, *pcvg);
+}
+
+extern void Object_ReloadSlots(z64_obj_ctxt_t* objectCtx);
+
+extern void* OVL_KaleidoScope_sPreRenderCvg;
+
+void Object_ReloadSlots_Heap(z64_obj_ctxt_t* objectCtx) {
+    for(int i = 0; i < objectCtx->numEntries; i++) {
+        int16_t id = objectCtx->slots[i].id;
+        uint32_t size = gObjectTable[id].vrom_end - gObjectTable[id].vrom_start;
+        if( i >= 3) {
+            objectCtx->slots[i].data = ZeldaArena_Malloc(size);
+        }
+        DmaMgr_RequestSync(objectCtx->slots[i].data, gObjectTable[id].vrom_start, size);
+    }
+}
+
+void KaleidoScope_FreeMemAndReloadSlots(z64_game_t* play) {
+    ZeldaArena_Free(play->pause_ctxt.icon_item);
+    ZeldaArena_Free(play->pause_ctxt.icon_item_24);
+    ZeldaArena_Free(play->pause_ctxt.icon_item_s);
+    ZeldaArena_Free(play->pause_ctxt.icon_item_lang);
+    ZeldaArena_Free(play->pause_ctxt.name_texture);
+    //OVL_KaleidoScope_sPreRenderCvg
+    void** sPreRenderCvg = (void**)resolve_kaleido_ovl_addr(&OVL_KaleidoScope_sPreRenderCvg);
+    ZeldaArena_Free(*sPreRenderCvg);
+    Object_ReloadSlots(&play->objectCtx);
+}
+
+extern uint32_t Player_InitPauseDrawData(z64_game_t* play, uint8_t* segment, SkelAnime* skelAnime);
+
+uint32_t KaleidoScope_Player_InitPauseDrawData_Hook(z64_game_t* play, uint8_t* segment, SkelAnime* skelAnime) {
+    // Free objects in the main object context that are allocated on the heap
+    // main keep, subkeep, and player are in their original places
+    // They should always be index 0, 1, and 2
+    for(int i = 3; i < play->objectCtx.numEntries; i++) {
+        ZeldaArena_Free(play->objectCtx.slots[i].data);
+    }
+    return Player_InitPauseDrawData(play, segment, skelAnime);
 }
--- ASM/src/hacks/ovl/ovl_kaleido_scope.asm
+++ ASM/src/hacks/ovl/ovl_kaleido_scope.asm
@@ -35,6 +35,125 @@
 .org 0x80820b38 ; offset 0xD378
     jal PauseMapMark_Draw_CallHook
 
+
+; Hack in KaleidoScope_Update to free the objects that are spawned on the heap
+; Just hook the call to Player_InitPauseDrawData and free everything there
+.org 0x808265AC
+; Replaces:
+;   jal     Player_InitPauseDrawData
+    jal     KaleidoScope_Player_InitPauseDrawData_Hook
+
+; Hack in KaleidoScope_Update init state to allocate space on the zeldaarena heap instead of object heap
+; Hack every DmaMgr_RequestSync call in that part of the code to do a malloc instead
+; Pass in pointers to the segments that we want to set instead
+; s0 has pauseCtx
+; Hack call to DmaMgr_RequestSync for pauseCtx->iconItemSegment
+; put address of pauseCtx->iconItemSegment into a0 instead of setting it
+.org 0x808265d8
+; Replaces:
+;   sw      a0, 0x128(s0)
+;   subu    a2, t8, a1
+;   jal     DmaMgr_RequestSync
+    addiu   a0, s0, 0x128
+    subu    a2, t8, a1
+    jal     KaleidoScope_AllocAndDmaRequest
+
+; Hack call to DmaMgr_RequestSync for pauseCtx->iconItem24Segment
+; put address of pauseCtx->iconItem24Segment into a0 instead of setting it
+.org 0x808266BC
+; Replaces:
+;   sw      a0, 0x12c(s0)
+;   subu    a3, t9, a1
+;   or      a2, a3, r0
+;   jal     DmaMgr_RequestSync
+    addiu   a0, s0, 0x12c
+    subu    a3, t9, a1
+    or      a2, a3, r0
+    jal     KaleidoScope_AllocAndDmaRequest
+
+; Hack calls to DmaMgr_RequestSync for pauseCtx->iconItemAltSegment
+; put address of pauseCtx->iconItemAltSegment into a0 instead of setting it
+.org 0x8082673C
+; Replaces:
+;   jal     DmaMgr_RequestSync
+;   lw      a0, 0x130(s0)
+    jal     KaleidoScope_AllocAndDmaRequest
+    addiu   a0, s0, 0x130
+.org 0x80826794
+; Replaces:
+;   jal     DmaMgr_RequestSync
+;   lw      a0, 0x130(s0)
+    jal     KaleidoScope_AllocAndDmaRequest
+    addiu   a0, s0, 0x130
+
+; Hack calls to DmaMgr_RequestSync for pauseCtx->iconItemLangSegment
+; put address of pauseCtx->iconItemLangSegment into a0 instead of setting it
+.org 0x808267e0
+; Replaces:
+;   jal     DmaMgr_RequestSync
+;   lw      a0, 0x134(s0)
+    jal     KaleidoScope_AllocAndDmaRequest
+    addiu   a0, s0, 0x134
+.org 0x80826808
+; Replaces:
+;   jal     DmaMgr_RequestSync
+;   lw      a0, 0x134(s0)
+    jal     KaleidoScope_AllocAndDmaRequest
+    addiu   a0, s0, 0x134
+
+; Hack calls to DmaMgr_RequestSync for pauseCtx->nameSegment
+; put address of pauseCtx->nameSegment into a0 instead of setting it
+; This one kinda stupid because it offsets by 0x400 to put the map
+; In KaleidoScope_UpdateNamePanel it DMAs the first 0x400
+; So what we want to do is allocate the full space for nameSegment here. We'll make this its own function
+; Then the stuff in KaleidoScope_UpdateNamePanel can stay as is
+.org 0x80826890
+; Replaces:
+;   jal     DmaMgr_RequestSync
+;   addiu   a0, a0, 0x400
+    jal     KaleidoScope_AllocAndDmaRequestNameSegment
+    addiu   a0, s0, 0x138
+.org 0x808268C8
+; Replaces:
+;   jal     DmaMgr_RequestSync
+;   addiu   a0, a0, 0x400
+    jal     KaleidoScope_AllocAndDmaRequestNameSegment
+    addiu   a0, s0, 0x138
+
+; Hack call to PreRender_SetValuesSave so we can allocate space for sPreRenderCvg
+; pass pointer to sPreRengerCvg in 0x14(sp)
+; this is stored in the overlay so we need to relocate it.
+; VRAM address 0x808301a8
+.org 0x808268f4
+; Replaces:
+;   lui     t7, 0x8083
+;   lw      t7, 0x01a8(t7)
+    lui     t7, 0x8083
+    addiu   t7, 0x01a8
+.org 0x80826914
+; Replaces:
+;   jal     PreRender_SetValuesSave
+    jal     KaleidoScope_PreRender_SetValuesSave_Hook
+
+; Hook the calls to the function that reloads all of the objects
+; So that we can free anything that we allocated for KaleidoScope
+; Function is named func_800981B8 in decomp and it's located at 800816b8
+; I'll call it Object_ReloadSlots
+
+.org 0x80828828
+; Replaces:
+;   addu    a0, s1, at
+;   jal     Object_ReloadSlots
+    or      a0, s1, r0 ; Pass play instead of objectctx
+    jal     KaleidoScope_FreeMemAndReloadSlots
+
+.org 0x80828b24
+; Replaces:
+;   addu    a0, s1, at
+;   jal     Object_ReloadSlots
+    or      a0, s1, r0 ; Pass play instead of objectctx
+    jal     KaleidoScope_FreeMemAndReloadSlots
+
 ; Relocs
 ; 0xD090
 .org 0x8082f62c
