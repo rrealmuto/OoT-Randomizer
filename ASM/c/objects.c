fbf5b69e292bd440f291cfed6c5b8fa11d078350
Rob Realmuto <rrealmuto@gmail.com>
2025-06-15 18:03:07 -0400

    Add misc models patcher

--- ASM/c/objects.c
+++ ASM/c/objects.c
@@ -51,5 +51,6 @@ void* Object_InitContext_AllocSpace(TwoHeadArena* tha, size_t size) {
     size -= LinkObjectVanillaSizes[z64_file.link_age];
     int16_t linkObjectId = gLinkObjectIds[z64_file.link_age];
     size += z64_object_table[linkObjectId].vrom_end - z64_object_table[linkObjectId].vrom_start;
+    size += 0x30000;
     return THA_AllocTailAlign16(tha, size);
 }
