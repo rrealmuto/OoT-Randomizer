80a59bd74b0544669195933868c2d12bb1380844
Rob Realmuto <rrealmuto@gmail.com>
2025-07-28 19:09:41 -0400

    EnOkarinaTag hacks to prevent cutscene playing

---
+++ ASM/c/en_okarina_tag.c
@@ -0,0 +1,16 @@
+#include "z64.h"
+#include "en_okarina_tag.h"
+#include "util.h"
+
+extern EnOkarinaTagActionFunc OVL_EnOkarinaTag_Action1;
+extern EnOkarinaTagActionFunc OVL_EnOkarinaTag_Action2;
+
+void EnOkarinaTag_ActionHook(EnOkarinaTag* this, z64_game_t* play) {
+    if(play->msgContext.ocarinaMode == 3 && this->type == 4 && !DemoKankyo_CutsceneFlags_Get_Hook(play, 2)) {
+        play->msgContext.ocarinaMode = 4;
+        this->actionFunc = resolve_overlay_addr(&OVL_EnOkarinaTag_Action1, this->actor.actor_id);
+        return;
+    }
+    EnOkarinaTagActionFunc EnOkarinaTag_Action = resolve_overlay_addr(&OVL_EnOkarinaTag_Action2, this->actor.actor_id);
+    EnOkarinaTag_Action(this, play);
+}
--- ASM/src/hacks.asm
+++ ASM/src/hacks.asm
@@ -4162,3 +4162,4 @@ DemoEffect_DrawJewel_AfterHook:
 .include "hacks/z_title.asm"
 .include "hacks/z_file_choose.asm"
 .include "hacks/ovl_en_changer.asm"
+.include "hacks/ovl_en_okarina_tag.asm"
